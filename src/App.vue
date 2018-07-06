f466a3be49459bfcd87c32cc99a7b21d1052f147
Dmitriy Blashchyk <dmitriy.blashchyk@gmail.com>
2018-07-06 17:09:53 +0300

    homepage

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,9 @@
               @click="drawer = !drawer"
               class="hidden-md-and-up"
       ></v-toolbar-side-icon>
-      <v-toolbar-title>My application</v-toolbar-title>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" class="pointer">My application</router-link>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn
@@ -64,3 +66,9 @@ export default {
 
 }
 </script>
+
+<style scoped>
+  .pointer {
+    cursor: pointer;
+  }
+</style>
