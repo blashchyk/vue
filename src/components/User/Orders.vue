8149bea06e62deacc3131da5b7ff7f2a460e9a5f
Dmitriy Blashchyk <dmitriy.blashchyk@gmail.com>
2018-07-06 17:38:20 +0300

    orders

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,38 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>Orders</h1>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text--secondary mb3" >Orders</h1>
+                <v-list
+                        subheader
+                        two-line
+                >
+
+                    <v-list-tile
+                            avatar
+                            v-for="order in orders"
+                            :key="order.id"
+                    >
+                        <v-list-tile-action>
+                            <v-checkbox
+                                :input-value="order.done"
+                                @change="markDone(order)"
+                                color="success"
+                            >
+
+                            </v-checkbox>
+                        </v-list-tile-action>
+
+                        <v-list-tile-content @click="notifications = !notifications">
+                            <v-list-tile-title>{{order.name}}</v-list-tile-title>
+                            <v-list-tile-sub-title>{{order.phone}}</v-list-tile-sub-title>
+                        </v-list-tile-content>
+                        <v-list-tile-action>
+                            <v-btn class="primary" :to="'/ad/' + order.id">Open</v-btn>
+                        </v-list-tile-action>
+                    </v-list-tile>
+
+                </v-list>
             </v-flex>
         </v-layout>
     </v-container>
@@ -11,7 +41,22 @@
 <script>
   export default {
     data () {
-      return {}
+      return {
+        orders: [
+          {
+            id: 'sck1',
+            name: 'Dmitriy',
+            phone: '050505055',
+            adId: '123',
+            done: false
+          }
+        ]
+      }
+    },
+    method: {
+      markDone (order) {
+        order.done = true
+      }
     }
   }
 </script>
