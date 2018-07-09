e21285ae4f017dbe8cd65a4d0a8eba9c2b2ce4f6
Dmitriy Blashchyk <dmitriy.blashchyk@gmail.com>
2018-07-09 15:00:50 +0300

    register_user

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 import Vuetify from 'vuetify'
+import * as fb from 'firebase'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
@@ -15,5 +16,15 @@ new Vue({
   router,
   store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created () {
+    fb.initializeApp({
+      apiKey: 'AIzaSyC7DkzLOVuIqap17y_llNg-zP_A4Jvkce0',
+      authDomain: 'itc-ads-5abbb.firebaseapp.com',
+      databaseURL: 'https://itc-ads-5abbb.firebaseio.com',
+      projectId: 'itc-ads-5abbb',
+      storageBucket: 'itc-ads-5abbb.appspot.com',
+      messagingSenderId: '280504655379'
+    })
+  }
 })
